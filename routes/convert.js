f41a113bedd2589b5ecab303f49a0ecf2afd585f
rifkimubarok <177006078@student.unsil.ac.id>
2019-12-01 10:53:26 +0700

    update

--- routes/convert.js
+++ routes/convert.js
@@ -1,17 +1,40 @@
 var express = require('express');
 var router = express.Router();
-var svg2img = require('svg2img');
+var http = require('http')
+var svg2img = require('./lib/convertsvg');
+var request = require('request').defaults({
+    encoding: null
+});
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
     svg2img(
         req.query.url,
         function (error, buffer) {
-            res.writeHead(200, {
-                'Content-Type': 'image/png'
-            });
-            res.write(buffer);
-            res.end()
+            console.log("error = ", error, "pass", buffer)
+            if (error == null) {
+                console.log("ini adalh errornya", error);
+                res.writeHead(200, {
+                    'Content-Type': 'image/png'
+                });
+                res.write(buffer);
+                res.end()
+
+            } else {
+                request.get(req.query.url, function (error, response, body) {
+                    if (!error && response.statusCode == 200) {
+                        res.writeHead(200, {
+                            'Content-Type': response.headers["content-type"],
+                            'Content-Length': response.headers["content-length"]
+                        });
+                        res.end(body);
+                    } else {
+                        res.send("error")
+                    }
+                });
+            }
+
+
         });
 });
 
